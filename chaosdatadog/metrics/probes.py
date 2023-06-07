451fc50dbc2b5e356c457a58e0644c375f4b84d4
Jorge Tapicha <jitapichab@gmail.com>
2023-06-07 16:04:43 -0500

    add module metrics to chaostoolkit-datadog
    Signed-off-by: jorge tapicha <jorge.tapicha@rappi.com>
    Signed-off-by: Jorge Tapicha <jitapichab@gmail.com>

--- chaosdatadog/metrics/probes.py
+++ chaosdatadog/metrics/probes.py
@@ -1,25 +1,25 @@
-from typing import Any, Dict, Literal
-from chaoslib.types import Configuration, Secrets
-from chaoslib.exceptions import ActivityFailed
-from datadog_api_client.v1.api.metrics_api import (
-    MetricsApi,
-)
 from datetime import datetime
+from typing import Literal
+
+from chaoslib.exceptions import ActivityFailed
+from chaoslib.types import Configuration, Secrets
+from datadog_api_client.v1.api.metrics_api import MetricsApi
 from dateutil.relativedelta import relativedelta
-from chaosdatadog import get_client
 from logzero import logger
+
+from chaosdatadog import get_client
 
 __all__ = ["get_metrics_state"]
 
 
 def get_metrics_state(
     query: str,
-    comparison: Literal['>', '<', '>=', '<=', '=='],
+    comparison: Literal[">", "<", ">=", "<=", "=="],
     threshold: float,
     minutes_before: int,
     configuration: Configuration = None,
-    secrets: Secrets = None
-) -> Dict[str, Any]:
+    secrets: Secrets = None,
+) -> bool:
     """
     The next function is to:
 
@@ -29,42 +29,39 @@ def get_metrics_state(
     * Check is the sum of datapoins is over some value.
       Ex. (requests, errors, custom metrics)
 
-    you can use a comparison to check if some data points in the query
-    do not meet the steady state condition
+    you can use a comparison to check if all data points in the query
+    satisfy the steady state condition
 
     Ex. cumsum(sum:istio.mesh.request.count.total{kube_service:test,
                response_code:500}.as_count())
 
-    the previous query is a cumulative sum of all the requests with response
-    code of 500. if you need to check that in a windows time your application
-    has more than 30 http_500 errors you comparison should be >.
+    the above query is a cumulative sum of all requests with response
+    code of 500. if you want your request in a window of time
+    you have a deviant hypothesis if you have more than 30 http_500 errors
+    the comparison should be <. so any value below 30 is a steady state.
 
-    if the treshold with the comparison matches with some data point the
-    hypothesis is going to have deviated state.
     """
 
     with get_client(configuration, secrets) as c:
         api = MetricsApi(c)
 
     metrics = api.query_metrics(
-                    _from=int(
-                        (
-                         datetime.now() + relativedelta(minutes=-minutes_before)
-                        ).timestamp()
-                    ),
-                    to=int(
-                           datetime.now().timestamp()
-                    ),
-                    query=query,
-                )
+        _from=int(
+            (
+                datetime.now() + relativedelta(minutes=-minutes_before)
+            ).timestamp()
+        ),
+        to=int(datetime.now().timestamp()),
+        query=query,
+    )
 
     metrics = metrics.to_dict()
     series = metrics.get("series", [{}])
     if not series:
         raise ActivityFailed("The query could not get points")
+
     series = series[0] if len(series) > 0 else {}
     point_list = series.get("pointlist", [])
     logger.info(point_list)
     point_value_list = [subpoints[1] for subpoints in point_list]
-    return not all(eval(f"_ {comparison} {threshold}")
-                   for _ in point_value_list)
+    return all(eval(f"_ {comparison} {threshold}") for _ in point_value_list)
